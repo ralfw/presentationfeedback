cfbe9f1b96514038305c9148efa53f8d96036d4c
Ralf Westphal <info@ralfw.de>
2015-01-26 08:02:15 +0100

    Class wire frame

---
+++ src/afapp/afapp.body/Body.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using EventStore.Contract;
+
+namespace afapp.body
+{
+	public class Body
+	{
+		Repository repo;
+		Conference conf;
+		Mapper map;
+
+		public Body (Repository repo, Conference conf, Mapper map) {
+			this.repo = repo;
+			this.conf = conf;
+			this.map = map;
+		}
+
+
+		public SessionOverviewVM GenerateSessionOverview(string confId) {
+
+		}
+	}
+
+	struct SessionOverviewVM {
+
+	}
+
+
+	public class Conference {
+
+	}
+
+
+	public class Repository {
+		public Repository(IEventStore es) {
+
+		}
+	}
+
+	public class ConferenceData {
+
+	}
+
+
+	public class Mapper {
+
+	}
+}
+
---
+++ src/afapp/afapp.head.console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace afapp.head.console
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
