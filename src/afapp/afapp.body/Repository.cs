5375578675a64095183dafbd9246958d58eebf7a
Ralf Westphal <info@ralfw.de>
2015-01-27 10:32:18 +0100

    Got increment working

--- src/afapp/afapp.body/Repository.cs
+++ src/afapp/afapp.body/Repository.cs
@@ -1,10 +1,10 @@
 using System;
 using EventStore.Contract;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace afapp.body
 {
-
 	public class Repository {
 		IEventStore es;
 
@@ -13,8 +13,40 @@ namespace afapp.body
 		}
 			
 		public ConferenceData LoadConference(string confId) {
-			throw new NotImplementedException ();
+			var events = this.es.QueryByContext (confId);
+
+			var confdata = new ConferenceData { Id = confId };
+			var sessionIds = new HashSet<string> ();
+			foreach (var e in events) {
+				switch (e.Name) {
+				case "ConferenceRegistered":
+					confdata.Title = e.Payload;
+					break;
+				case "SessionAssigned":
+					sessionIds.Add (e.Payload);
+					break;
+				}
+			}
+
+			events = this.es.QueryByName ("SessionRegistered")
+							.Where (e => sessionIds.Contains (e.Context));
+			var confSessions = new List<SessionData> ();
+			foreach (var e in events) {
+				var fields = e.Payload.Split ('\t');
+
+				var sessiondata = new SessionData{ 
+					Id = e.Context,
+					Title = fields[0],
+					Start = DateTime.Parse(fields[1]),
+					End = DateTime.Parse(fields[2]),
+					SpeakerName = fields[3],
+					SpeakerEmail = fields[4]
+				};
+				confSessions.Add (sessiondata);
+			}
+
+			confdata.Sessions = confSessions;
+			return confdata;
 		}
 	}
-
 }
--- src/afapp/afapp.head.console/Head.cs
+++ src/afapp/afapp.head.console/Head.cs
@@ -13,15 +13,21 @@ namespace afapp.head.console
 			this.body = body;
 		}
 
-		public void Run(string[] args) {
-			var vm = this.body.GenerateSessionOverview (args [0]);
 
-			Console.WriteLine ("# Sessions of {0} ({1})", vm.ConfTitle, vm.ConfId);
-			Console.WriteLine ("## Active");
-			Display_sessions (vm.ActiveSessions);
-			Console.WriteLine ("## Inactive");
-			Display_sessions (vm.InactiveSessions);
+		public void Run(string[] args) {
+			switch (args [0]) {
+			case "overview":
+				var vm = this.body.GenerateSessionOverview (args [1]);
+
+				Console.WriteLine ("# Sessions of {0} ({1})", vm.ConfTitle, vm.ConfId);
+				Console.WriteLine ("## Active");
+				Display_sessions (vm.ActiveSessions);
+				Console.WriteLine ("## Inactive");
+				Display_sessions (vm.InactiveSessions);
+				break;
+			}
 		}
+
 
 		void Display_sessions(IEnumerable<SessionVM> sessions) {
 			foreach (var s in sessions)
