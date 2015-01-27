b26e7315f4984d0d7f2b20cbad91ebf8cd73e4e7
Ralf Westphal <info@ralfw.de>
2015-01-27 11:07:58 +0100

    Added time provider command line argument
    use -now:YYYY-MM-DDTHH:MM:SS to pass the current time to afapp

--- src/afapp/afapp.head.console/Program.cs
+++ src/afapp/afapp.head.console/Program.cs
@@ -2,6 +2,7 @@
 using afapp.body;
 using EventStore;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace afapp.head.console
 {
@@ -11,10 +12,19 @@ namespace afapp.head.console
 		{
 			var es = new FileEventStore ("app.events");
 			var repo = new Repository (es);
-			var body = new Body (repo, () => DateTime.Now);
+			var body = new Body (repo, BuildCurrentTimeProvider(args));
 			var head = new Head (body);
 
 			head.Run (args);
+		}
+			
+		private static Func<DateTime> BuildCurrentTimeProvider(string[] args) {
+			const string NOW_PREFIX = "-now:";
+			var nowArg = args.FirstOrDefault (a => a.StartsWith(NOW_PREFIX));
+			if (nowArg == null)	return () => DateTime.Now;
+
+			var currentTime = DateTime.Parse (nowArg.Substring (NOW_PREFIX.Length));
+			return () => currentTime;
 		}
 	}
 }
